53dab8f0b1a332a3df7635da8313b6aed7a177bf
NanneHuiges <nanne@huiges.nl>
2019-02-01 22:41:45 +0100

    retrieves on/off from domoticz

--- src-grid/main.cpp
+++ src-grid/main.cpp
@@ -16,15 +16,19 @@
 #include <NeoPixelAnimator.h>
 // this includes some form of time.h it seems
 #include <NtpClientLib.h>
+// http client
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
 // the 437 character definitions.
 #include "matrix_font.h"
 
 // 'brightness' of the led. 0 - 255 (?)
 #define colorSaturation 32
 
-const char* SSID = "ledgrid clock";
+const char *SSID = "ledgrid clock";
 
-// set the matrix 
+// set the matrix
 typedef ColumnMajorAlternatingLayout MyPanelLayout;
 const uint8_t PanelWidth = 16; // 8 pixel x 8 pixel matrix of leds
 const uint8_t PanelHeight = 16;
@@ -45,7 +49,7 @@ RgbColor yellow(colorSaturation, colorSaturation, 0);
 RgbColor orange(colorSaturation, colorSaturation / 2, 0);
 RgbColor purple(colorSaturation / 8, 0, colorSaturation);
 RgbColor green(0, colorSaturation, 0);
-RgbColor lgreen(colorSaturation/4, colorSaturation/2, 0);
+RgbColor lgreen(colorSaturation / 4, colorSaturation / 2, 0);
 RgbColor blue(0, 0, colorSaturation);
 RgbColor white(colorSaturation);
 RgbColor black(0);
@@ -53,11 +57,12 @@ RgbColor black(0);
 // the 'current' leds to write to the matrix
 RgbColor leds[PanelHeight][PanelWidth] = {green};
 
-void setup_wifi() {
-  // maak een access point om je wifi netwerk in te kunnen stellen
-  WiFiManager wifiManager;
-  wifiManager.autoConnect(SSID); 
-  Serial.println(WiFi.localIP());
+void setup_wifi()
+{
+    // maak een access point om je wifi netwerk in te kunnen stellen
+    WiFiManager wifiManager;
+    wifiManager.autoConnect(SSID);
+    Serial.println(WiFi.localIP());
 }
 
 /**
@@ -76,7 +81,7 @@ void setup()
 
     Serial.println();
     Serial.println("Initializing...");
-    setup_wifi(); 
+    setup_wifi();
     strip.Begin();
     strip.Show();
     NTP.begin("pool.ntp.org", 1, true, 0);
@@ -111,9 +116,12 @@ void set_character(unsigned char letter, RgbColor color, int quadrant)
         for (byte y = 0; y < 8; y++)
         {
             bool led = ((row_byte >> y) & 0x01);
-            if(led == 1){
+            if (led == 1)
+            {
                 leds[y + (8 * quadrant_y)][x + (8 * quadrant_x)] = color;
-            }else{
+            }
+            else
+            {
                 leds[y + (8 * quadrant_y)][x + (8 * quadrant_x)] = black;
             }
         }
@@ -122,12 +130,40 @@ void set_character(unsigned char letter, RgbColor color, int quadrant)
 
 void set_sec(int sec, RgbColor color)
 {
-    for (int y = 0; y < sec % 10; y++){
+    for (int y = 0; y < sec % 10; y++)
+    {
         leds[y][PanelWidth - 1] = color;
     }
-    for (int y = 0; y < sec/10; y++){
+    for (int y = 0; y < sec / 10; y++)
+    {
         leds[y][PanelWidth - 2] = color;
     }
+}
+
+bool should_display()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        String url = "http://192.168.178.2:8084/json.htm?type=devices&rid=158";
+        http.begin(url);           
+        int httpCode = http.GET(); 
+        if (httpCode == 200)
+        {                                      
+            String payload = http.getString(); 
+            DynamicJsonBuffer jsonBuffer;
+            JsonObject &root = jsonBuffer.parseObject(payload);
+            // Unfortunately, the following doesn't work (issue #118):
+            // sensor = root["sensor"]; // <-  error "ambiguous overload for 'operator='"
+            // As a workaround, you need to replace by
+            String data = root["result"][0]["Data"].as<String>();
+            return data == "On";
+        }
+        Serial.println("no status 200 from domoticz");
+        return false;
+    }
+    Serial.println("no wifi connected");
+    return false;
 }
 
 /**
@@ -135,7 +171,9 @@ void set_sec(int sec, RgbColor color)
  */
 void loop()
 {
-    if(WiFi.status()== WL_CONNECTED){
+
+    if (should_display())
+    {
         time_t t = now();
 
         int h = (t / 3600) % 24;
@@ -150,19 +188,18 @@ void loop()
         char strm2[2];
         sprintf(strm2, "%d", m % 10);
 
-        int s = t % 60;
+        set_character(strh1[0], blue, lefttop);
+        set_character(strh2[0], blue, righttop);
+        set_character(strm1[0], purple, leftbot);
+        set_character(strm2[0], purple, rightbot);
 
-        set_character(strh1[0], blue,  lefttop);
-        set_character(strh2[0], blue,  righttop);
-        set_character(strm1[0], purple,  leftbot);
-        set_character(strm2[0], purple,  rightbot);
-
-        set_sec(s, lgreen);
+        // int s = t % 60;
+        // set_sec(s, lgreen);
 
         show();
-
     } else {
-        Serial.println("No wifi");
+        strip.ClearTo(black);
+        strip.Show();
     }
     delay(1000);
 }
